74d1740405aabec601e008d5dd0e0943280db117
Kaitou Ryz <faryz.coder@gmail.com>
2020-03-24 02:05:44 +0800

    link store retailer login

--- app/src/main/java/com/example/p_navadmin/Login_ShopRetailer.kt
+++ app/src/main/java/com/example/p_navadmin/Login_ShopRetailer.kt
@@ -1,13 +1,47 @@
 package com.example.p_navadmin
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.getField
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.login_store_retailer.*
 
 class Login_ShopRetailer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_store_retailer)
 
+        val db = Firebase.firestore
 
+        buttonLoginSR.setOnClickListener {
+
+            // continue if username and password are not empty
+            if (inputUsernameSR.text.isNotEmpty() && inputPasswordSR.text.isNotEmpty()) {
+
+                db.collection("Login").document("store retailer")
+                    .get()
+                    .addOnSuccessListener { result ->
+                        d("firebase", "SR username ${result.getField<String>("username")}")
+                        d("firebase", "SR password ${result.getField<String>("password")}")
+                        val usernameSR = result.getField<String>("username")
+                        val passwordSR = result.getField<String>("password")
+                        val iUsernameSR = inputUsernameSR.text.toString()
+                        val iPasswordSR = inputPasswordSR.text.toString()
+
+                        if (usernameSR.equals(iUsernameSR) && passwordSR.equals(iPasswordSR)) {
+                            startActivity(Intent(this, Shop_Retailer_Promotion_Event::class.java))
+                        }
+                        else {
+                            // if false
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        d("firebase", "error SR ", e)
+                    }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/p_navadmin/Shop_Retailer_Promotion_Event.kt
@@ -0,0 +1,11 @@
+package com.example.p_navadmin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Shop_Retailer_Promotion_Event : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shop_retailer_promotion)
+    }
+}
